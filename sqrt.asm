908bbd111d2a13732d7a42ce987e33388dce93aa
Haley <hlind.engineering@gmail.com>
2025-09-06 16:22:42 -0400

    adding beginnings of conditional statements, exit conditions; further code cleanup for readability

--- sqrt.asm
+++ sqrt.asm
@@ -2,29 +2,30 @@
 nl:     .asciz      "\n"
 
         .text
-        li    s0, 0       # initialize GUESS at 0 (arbitrary) 
-        li    s2, 256     # STEP (start at halfway through) ... 256 with 14 fractional bits because s1 is fixed point
-        slli  s2, s2, 14 # shifts to being a full
+        li      s0, 0           # initialize GUESS at 0 (arbitrary) 
+        li      s2, 256         # STEP (start at halfway through) ... 256 with 14 fractional bits because s1 is fixed point
+        slli    s2, s2, 14      # shifts to being a full
       
         # system input
-        li    a7, 5       # system call, reading an int from input console
-        ecall # stops program until user presses enter
+        li      a7, 5       # system call, reading an int from input console
+        ecall                   # stops program until user presses enter
 
-loop:   # Computing guess^2 
-        mulhu       s1, a0, a0 # UPPER 32 Bit mulhu (UNSIGNED) vs mul: mulh is upper 32 bits / mul VS mulh: mulh gives upper half, mul gives lower 
-        mul   a1, a0, a0 # recovers LOWER 32 of the produt
-        # a0:a1 is the 64 bit number
+loop:   # Computing guess^2 (18.14 * 18.14 = 36.28 bits)
+        mulhu   s1, a0, a0      # recovers UPPER 32 Bit | general:  mulh/mulhu gives higher; mul gives lower 
+        mul     a1, a0, a0      # recovers LOWER 32 of the produt
+                                # s1:a1 is now the 64 bit number
+                                # bit shift it to recover to 32 (18.14) guess
+        srli    a1, a1, 14      # upper
+        slli    s1, s1, 18      # lower
+        or      a2, a1, s1      # result is a 32 bit fixed point value; 18 upper, 14 fractional bits
       
-        # bit shift it to recover to 32 (18.14) guess
-        srli a1, a1, 14 # upper
-        slli s1, s1, 18 # lower
-        or a2, a1, s1
-      
-        # compare guess with input value
-        # bge  # <a> <b> <c> ; if a >= b, then c ... greater than or equal to 
-        bge   a2 a0 subtract #subtract step from guess # compare guess squared to input value # then divide by 2
-        ble   a2 a0 add # if guess^2 is less than input ... 
-        # we need to add the step to our guess
+        # NEXT: compare guess with input value
+                # recall a0 holds user input, and we assigned the guess (18.14) to register a2
+                # if guess (a2) greater than input (a0), 
+
+        bge   a2 a0 subtract    #subtract step from guess # compare guess squared to input value # then divide by 2
+        ble   a2 a0 add         # if guess^2 is less than input ... 
+                                # we need to add the step to our guess
             
         #subtract step from guess # compare guess squared to input value # then divide by 2
         # add ot subtract
@@ -34,17 +35,19 @@ loop:   # Computing guess^2
         # if guess^2 and original are equal
       
       
-add:    addi s0 s0 s2 # guess = step added to guess (we are adding a step bc guess is too low) 
-        srli s2 s2 1 # divide our guess by 2, the new value overwrites old 
-            
+add:    addi    s0 s0 s2        # guess (a2) = step added to guess (we are adding a step bc guess is too low) 
+        srli    s2 s2 1         # divide our step (s2) by 2, the new value overwrites old 
+        beq     s2, x0, exit
+    
 subtract:   
-        # subtract step to guess;     
+        # subtract step to guess;   
+        beq     s2, x0, exit    
+        # j is unconditional jump statement ... use it!  
       
 exit:   # print out answer with sys call (if (guess^2 with input) equal or equals 0)
-        # ecall
-      
-        # exit:     a7, 5
-        # ecall
+        # TODO: Print out answer and newline
+
+        li      a7, 10 #system call (a7) # 10 is exit
       
       
        
